60208d571b389206e6816e46cfa668fdadb111a6
Allen_K <allenkelly278@gmail.com>
2021-04-10 10:08:03 +0100

    test Song class passing

--- tests/test_song.py
+++ tests/test_song.py
@@ -8,4 +8,16 @@ class TestSong(unittest.TestCase):
     def test_song_has_name(self):
         self.assertEqual("Thunderstruck", self.rock.name)
 
+    def test_song_has_artist(self):
+        self.assertEqual("AC/DC", self.rock.artist)
+
+    def test_song_has_duration(self):
+        self.assertEqual(292, self.rock.duration)
+
+    def test_song_has_release_date(self):
+        self.assertEqual(1990, self.rock.year)
+
+    def test_song_has_genre(self):
+        self.assertEqual("Rock", self.rock.genre)
+
     
