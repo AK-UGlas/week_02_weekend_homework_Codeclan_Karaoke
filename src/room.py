1a183c88a13dd0898c284c03e78f959a22420383
Allen_K <allenkelly278@gmail.com>
2021-04-11 14:04:43 +0100

    MVP complete. all tests passing

--- src/room.py
+++ src/room.py
@@ -6,18 +6,28 @@ class Room:
         self.songs = []
         
     def check_in(self, guest):
+        # ensure guest isn't already in the room
+        if self.guest_in_room(guest):
+            return f"{guest.name} has already checked in"
+
+        # make sure room has adequate capacity for another guest
         if len(self.guest_list) < self.capacity:
             self.guest_list.append(guest)
             return f"{guest.name} checked in to {self.name}"
         
-        return "Cannot add guest. Room is full"
+        return "Cannot add guest: Room is full"
         
     def check_out(self, guest):
-        if guest in self.guest_list:
+        if self.guest_in_room(guest):
             self.guest_list.remove(guest)
             return f"{guest.name} has left {self.name}"
+        
+        return f"{guest.name} is not in {self.name}"
 
-    def add_song(song):
+    def guest_in_room(self, guest):
+        return guest in self.guest_list
+
+    def add_song(self, song):
         if song not in self.songs:
             self.songs.append(song)
 
--- tests/test_room.py
+++ tests/test_room.py
@@ -18,6 +18,16 @@ class TestRoom(unittest.TestCase):
         self.medium = Room("Beats room", 10)
         self.small = Room("Chillout room", 5)
 
+        self.acdc_fan = Guest("Allen", self.playlist[0])
+
+        self.party = [
+                Guest("John", self.playlist[0]),
+                Guest("Paul", self.playlist[1]),
+                Guest("George", self.playlist[2]),
+                Guest("Ringo", self.playlist[3]),
+                Guest("Stuart", self.playlist[4]),
+        ]
+
     # Test room attributes
     def test_room_has_name(self):
         self.assertEqual("Party room", self.large.name)
@@ -25,6 +35,51 @@ class TestRoom(unittest.TestCase):
     def test_room_has_capacity(self):
         self.assertEqual(15, self.large.capacity)
 
+    def test_room_guest_list_empty(self):
+        self.assertEqual(0, len(self.large.guest_list))
+
+    def test_room_song_list_empty(self):
+        self.assertEqual(0, len(self.large.songs))
+
+    # Test room methods
+    # 1: checking guests in
+    def test_check_in_guest_to_room(self):
+        self.assertEqual("Allen checked in to Party room", 
+                         self.large.check_in(self.acdc_fan))
+
+    def test_guest_is_already_in_room(self):
+        self.large.check_in(self.acdc_fan)
+        self.assertEqual("Allen has already checked in", self.large.check_in(self.acdc_fan))
+        self.assertEqual(1, len(self.large.guest_list))
+
+    def test_room_is_at_capacity(self):
+        for guest in self.party:
+            self.small.check_in(guest)
+
+        self.assertEqual("Cannot add guest: Room is full", self.small.check_in(self.acdc_fan))
+            
+    # 2: checking guest out
+    def test_guest_check_out_of_room(self):
+        self.large.check_in(self.acdc_fan)
+        self.assertEqual("Allen has left Party room", 
+                         self.large.check_out(self.acdc_fan))
+
+    def test_guest_not_in_room(self):
+        self.large.check_in(self.acdc_fan)
+        self.assertFalse(self.large.guest_in_room(self.party[0]))
+
+    def test_guest_in_room(self):
+        self.large.check_in(self.acdc_fan)
+        self.assertTrue(self.large.guest_in_room(self.acdc_fan))
+
+    # 3: adding songs
+    def test_add_song_to_room(self):
+        self.large.add_song(self.playlist[0])
+        self.assertEqual(1, len(self.large.songs))
+
+    def test_song_already_added(self):
+        self.large.add_song(self.playlist[0])
+        self.assertTrue(len(self.large.songs) == 1)
 
 
 
